3320b2c3eb1e96313a7201c1a7c3b2ab7a7d83ac
Slobodan Dan <danastroman@gmail.com>
2019-01-10 22:51:43 +0100

    Content style and object creation refactoring

--- Script/Index.js
+++ Script/Index.js
@@ -1,40 +1,78 @@
 var Books = (function(){
 
-	let br = function() {
-		return document.createElement("br");
+	let box = function(e) {
+		let div = document.createElement("div");
+		div.appendChild(e);
+		return div;
+	}
+
+	let domImage = function(src, alt) {
+		let img = document.createElement("img");
+		img.src = src;
+		img.alt = alt;
+		return img;
+	}
+
+	let domLink = function(href, text) {
+		let a = document.createElement("a");
+		a.href = href;
+		a.innerHTML = text;
+		return a;
+	}
+
+	let bookImage = function(book) {
+		let a = document.createElement("a")
+		a.href = `BookDetails.html?id=${book.id}`;
+		let image = domImage(book.cover, book.title);
+		a.appendChild(image);
+		let imageBox = box(a);
+		imageBox.classList = "image";
+		return imageBox;
+	}
+
+	let bookTitle = function(book) {
+		let title = domLink(`BookDetails.html?id=${book.id}`,
+			`${book.title} (${book.year})`);
+		let titleBox = box(title);
+		titleBox.classList = "title";
+		return titleBox;
+	}
+
+	let bookAuthor = function(book) {
+		let authorLink = domLink(`Author.html?author=${book.author}`,
+			book.author);
+		let authorBox = box(authorLink);
+		authorBox.classList = "author";
+		return authorBox;
+	}
+
+	let bookPrice = function(book) {
+		let price = document.createElement("p");
+		price.innerHTML = `$${book.price}`;
+		price.id = `price${book.id}`;
+		let priceBox = box(price);
+		priceBox.classList = "price";
+		return priceBox;
 	}
 
 	let createBook = function(book) {
 
-		let imageLink = document.createElement("a");
-		imageLink.href = `BookDetails.html?id=${book.id}`;
+		let image = bookImage(book);
 
-		let img = document.createElement("img");
-		img.src = book.cover;
-		img.alt = book.title;
-		img.classList = "pic";
-		imageLink.appendChild(img);
+		let title = bookTitle(book);
+		let author = bookAuthor(book);
+		let price = bookPrice(book);
 
-		let price = document.createElement("p");
-		price.innerHTML = `$${book.price}`;
-		price.id = `price${book.id}`;
-		
-		let title = document.createElement("a");
-		title.innerHTML = `${book.title} (${book.year})`;
-		title.href = `BookDetails.html?id=${book.id}`;
-
-		let author = document.createElement("a");
-		author.innerHTML = book.author;
-		author.href = `Author.html?author=${book.author}`;
+		let textBox = document.createElement("div");
+		textBox.classList = "text";
+		textBox.appendChild(title);
+		textBox.appendChild(author);
+		textBox.appendChild(price);
 
 		let li = document.createElement("li");
 		li.classList = "content";
-		li.appendChild(imageLink);
-		li.appendChild(br());
-		li.appendChild(title);
-		li.appendChild(br());
-		li.appendChild(author);
-		li.appendChild(price);
+		li.appendChild(image);
+		li.appendChild(textBox);
 
 		return li;
 	}
@@ -65,7 +103,6 @@ var Books = (function(){
 
 				let title = li.getElementsByTagName("img")[0].alt
 				title = title.toLowerCase();
-				console.log(title, value);
 				if (!title.startsWith(value))
 					li.style.display = "none";
 			}
