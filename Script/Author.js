9fee4c453bc064f1b6a529f00537a1a89dda7784
Slobodan Dan <danastroman@gmail.com>
2019-01-12 21:35:03 +0100

    Added pre-loaded authors

--- Script/Author.js
+++ Script/Author.js
@@ -1,11 +1,29 @@
 var Author = (function() {
 
+	let getId = function(author) {
+		return `${author.firstname.toLowerCase()}_${author.lastname.toLowerCase()}`;
+	}
+
+	if (Persistence.Get("authors") == undefined) {
+		let asimov = { id : "isaac_asimov", firstname : "Isaac", lastname : "Asimov" };
+		let bulgakov = { id : "mikhail_bulgakov", firstname : "Mikhail" , lastname : "Bulgakov" };
+		let loe = { id : "elrlend_loe", firstname : "Erlend", lastname : "Loe" };
+		let authors = { asimov, bulgakov, loe };
+		Persistence.Set("authors", authors);
+	}
+
 	return {
 		Save : function(form) {
 			let author = Form.Serialize(form);
-			let id = author.firstname.toLowerCase() 
-				+ author.lastname.toLowerCase();
+			id = getId(author);
+			authors = Persistence.Get("authors");
+			if (authors[id] != undefined) {
+				alert(`Author ${author.firstname} ${author.lastname} is already added.`);
+				return false;
+			}
+
 			Persistence.Add("authors", id, author);
+			return true;
 		},
 		Load : function(select, selectedAuthor) {
 			let authors = Persistence.Get("authors");
